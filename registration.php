b5fe806d7eb1376030cf45360c9d62bf1a688423
thymo <thymo.liempt@rocva.nl>
2023-01-25 11:03:20 +0100

    rolled back php connection

--- registration.php
+++ registration.php
@@ -2,22 +2,6 @@
 <html lang="en">
 <body>
 <style>
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "MYsql10#$";
-    $database = "";
-    $port = 3308;
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $database, $port);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
-    ?>
     body {
         font-family: Arial;
     }
@@ -115,4 +99,4 @@ function test_input($data) {
 ?>
 
 </body>
-</html>
+</html
