75021cd6718f71f2e74f441b28b687125cd479f1
thymo <thymo.liempt@rocva.nl>
2023-01-25 10:49:14 +0100

    added php connection to database

--- registration.php
+++ registration.php
@@ -2,7 +2,22 @@
 <html lang="en">
 <body>
 <style>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "MYsql10#$";
+    $database = "";
+    $port = 3308;
 
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database, $port);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+    ?>
     body {
         font-family: Arial;
     }
